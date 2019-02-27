706610c3d7fa6a50d410a584fa563913ccac0f16
Sebastiaan Hekner <sebastiaan.hekner@secrid.com>
2019-02-27 13:49:24 +0100

    Test with file write

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 8010;
 
-app.get('/', (req, res, next) => res.send('Hello world! You are now connected :)'));
+app.get('/', (req, res, next) => {
+  const filename = new Date().toLocaleTimeString() + '.txt';
+  fs.writeFile('./test-data/' + filename, 'Hello content! This is the date: ' + filename, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  res.send('Hello world! You are now connected :)')
+});
 
 app.listen(port, () => console.log('server is running'));
